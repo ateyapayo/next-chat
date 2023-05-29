cd7db836e42440fb66b8c42919d1c58da9aaac80
ateyapayo <andrea.piano@softfobia.com>
2023-05-29 11:52:28 +0200

    feat: first commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { Inter } from "next/font/google";
+
+import ToasterContext from "./context/ToasterContext";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "NextChat",
+  description: "Next Generation Chat - developed by Andrea Piano",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ToasterContext />
+        {children}
+      </body>
+    </html>
+  );
+}
