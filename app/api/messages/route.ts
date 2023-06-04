8045ff11abe8848318d42507bd82f677d47700a7
Andrea Piano <andreapiano@Andreas-MacBook-Pro.local>
2023-06-04 22:27:20 +0200

    feat: conversation template

---
+++ app/api/messages/route.ts
@@ -0,0 +1,21 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(request: Request) {
+  try {
+    const currentUser = await getCurrentUser();
+    const body = await request?.json();
+
+    const { message, image, conversationId } = body;
+
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const newMessage = await prisma;
+  } catch (error) {
+    console.log(error, "ERROR_MESSAGES");
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
